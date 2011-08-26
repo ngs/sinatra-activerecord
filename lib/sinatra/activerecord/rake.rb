747c47639164f31df834c750123ab1e9f8dd4a99
Vladimir <penkinv@gmail.com>
2011-08-26 16:06:25 +0400

    db:rollback db:reset db:seed db:drop

--- lib/sinatra/activerecord/rake.rb
+++ lib/sinatra/activerecord/rake.rb
@@ -87,6 +87,37 @@ namespace :db do
     )
   end
 
+  desc 'Rolls the schema back to the previous version (specify steps w/ STEP=n).'
+  task :rollback do
+    step = ENV['STEP'] ? ENV['STEP'].to_i : 1
+    ActiveRecord::Migrator.rollback('db/migrate/', step)
+  end
+
+
+  desc "reset database"
+  task :reset => ["db:drop", "db:create", "db:migrate", "db:seed"]
+
+  desc 'Load the seed data from db/seeds.rb'
+  task :seed => 'db:abort_if_pending_migrations' do
+    seed_file = 'db/seeds.rb'
+    load(seed_file) if File.exist?(seed_file)
+  end
+
+  task :abort_if_pending_migrations => :load_config do
+    if defined? ActiveRecord
+      pending_migrations = ActiveRecord::Migrator.new(:up, 'db/migrate').pending_migrations
+
+      if pending_migrations.any?
+        puts "You have #{pending_migrations.size} pending migrations:"
+        pending_migrations.each do |pending_migration|
+          puts '  %4d %s' % [pending_migration.version, pending_migration.name]
+        end
+        abort %{Run "rake db:migrate" to update your database then try again.}
+      end
+    end
+  end
+
+
   desc "create an ActiveRecord migration in ./db/migrate"
   task :create_migration do
     name = ENV['NAME']
@@ -111,4 +142,39 @@ namespace :db do
       EOS
     end
   end
+
+  namespace :drop do
+    # desc 'Drops all the local databases defined in config/database.yml'
+    task :all => :load_config do
+      ActiveRecord::Base.configurations.each_value do |config|
+        # Skip entries that don't have a database key
+        next unless config['database']
+        begin
+          # Only connect to local databases
+          local_database?(config) { drop_database(config) }
+        rescue Exception => e
+          $stderr.puts "Couldn't drop #{config['database']} : #{e.inspect}"
+        end
+      end
+    end
+  end
+
+  desc 'Drops the database for the current RACK_ENV (use db:drop:all to drop all databases)'
+  task :drop => :load_config do
+    config = ActiveRecord::Base.configurations[RACK_ENV || 'development']
+    begin
+      drop_database(config)
+    rescue Exception => e
+      $stderr.puts "Couldn't drop #{config['database']} : #{e.inspect}"
+    end
+  end
+
+  def local_database?(config, &block)
+    if %w( 127.0.0.1 localhost ).include?(config['host']) || config['host'].blank?
+      yield
+    else
+      $stderr.puts "This task only modifies local databases. #{config['database']} is on a remote host."
+    end
+  end
+
 end
